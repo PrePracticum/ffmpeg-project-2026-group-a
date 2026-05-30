c9387fb4053ecf182a9a733b400815f982cb5daa
Rivki Karp <r48567381@gmail.com>
2026-05-30 21:47:46 +0300

    add command Revese video

---
+++ FFmpeg.Core/Models/BrightnessContrastModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FFmpeg.Core.Models
+{
+    public class BrightnessContrastModel
+    {
+        public string InputFile { get; set; } = string.Empty;
+        public string OutputFile { get; set; } = string.Empty;
+        public double Brightness { get; set; }
+        public double Contrast { get; set; }
+    }
+}
