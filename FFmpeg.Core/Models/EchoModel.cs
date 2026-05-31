6a23158e96549172e7f3e71c6881bcd91e9d6380
Yael Cohen <yael3222664@gmail.com>
2026-05-31 14:05:54 +0300

    Adding an echo effect to the video audio

---
+++ FFmpeg.Core/Models/EchoModel.cs
@@ -0,0 +1,9 @@
+namespace FFmpeg.Core.Models
+{
+    public class EchoModel 
+    {
+        public string InputFile { get; set; }
+        public string OutputFile { get; set; }
+        public string Duration { get; set; } 
+    }
+}
