0e0b49949d037980040f169c2bc701a964473ca9
MalkaSafra <malki188640@gmail.com>
2026-06-04 17:29:25 +0300

    remove audio

---
+++ FFmpeg.Core/Models/AudioRemovalModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFmpeg.Core.Models
+{
+    public class AudioRemovalModel
+    {
+        public required string InputFile { get; set; }
+        public required string OutputFile { get; set; }
+        public string VideoCodec { get; set; } = "libx264";
+    }
+}
---
+++ FFmpeg.Infrastructure/Commands/AudioRemovalCommand.cs
@@ -0,0 +1,35 @@
+﻿using Ffmpeg.Command.Commands;
+using FFmpeg.Core.Models;
+using FFmpeg.Infrastructure.Commands;
+using FFmpeg.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ffmpeg.Command.Commands
+{
+    public class AudioRemovalCommand : BaseCommand, ICommand<AudioRemovalModel>
+    {
+        private readonly ICommandBuilder _commandBuilder;
+
+        public AudioRemovalCommand(FFmpegExecutor executor, ICommandBuilder commandBuilder)
+            : base(executor)
+        {
+            _commandBuilder = commandBuilder ?? throw new ArgumentNullException(nameof(commandBuilder));
+        }
+
+        public async Task<CommandResult> ExecuteAsync(AudioRemovalModel model)
+        {
+         
+            CommandBuilder = _commandBuilder
+                .SetInput(model.InputFile)
+                .SetVideoCodec(model.VideoCodec)
+                .AddOption("-an")
+                .SetOutput(model.OutputFile, false);
+
+            return await RunAsync();
+        }
+    }
+}
